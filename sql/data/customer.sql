4f92c0cc022206bf80b02a532c13b3252aba7a41
mchopra <manan.chopra@gmail.com>
2020-06-05 18:01:53 -0400

    Add customer dataset

---
+++ sql/data/customer.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customer (customer_id INTEGER, last_name VARCHAR(50), first_name VARCHAR(50), favorite_website VARCHAR(50));
+INSERT INTO customer VALUES(4000,'Jackson Joe',	'google.com');
+INSERT INTO customer VALUES(5000,'Smith	Jane','facebook.com');
+INSERT INTO customer VALUES(6000,'Ferguson Samantha', 'amazon.com');
+INSERT INTO customer VALUES(7000,'Reynolds Allen','apple.com');
+INSERT INTO customer VALUES(8000,'Anderson Paige',NULL);
+INSERT INTO customer VALUES(9000,'Johnson Derek','instagram.com');
