e339d95cdd6486af729d3f89661667747b30229c
mchopra <manan.chopra@gmail.com>
2020-06-05 20:15:29 -0400

    Add employee data set

---
+++ sql/data/employee.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employee (City VARCHAR(50), Department VARCHAR(50), Employee_id INTEGER, Salary DECIMAL(10,2));
+INSERT INTO employee VALUES('Houston','Sales',12345,50000);
+INSERT INTO employee VALUES('Houston','Sales',12346,10000);
+INSERT INTO employee VALUES('Houston','Marketing',12347,50000);
+INSERT INTO employee VALUES('Denver','Sales',12358,60000);
+INSERT INTO employee VALUES('Denver','Sales',12349,60000);
+INSERT INTO employee VALUES('Denver','Marketing',12350,75000);
