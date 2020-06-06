ed092e89d06aa032e5223e74ccefb6b0ff69d4f5
mchopra <manan.chopra@gmail.com>
2020-06-05 20:32:04 -0400

    Add employee_dob file

---
+++ sql/data/employee_dob.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employee_dob (employee_id INTEGER, date_of_birth DATE);
+INSERT INTO employee_dob VALUES (12345,	'1990-02-02');
+INSERT INTO employee_dob VALUES (12346,	'1992-05-10');
+INSERT INTO employee_dob VALUES (12347,	'1995-06-08');
+INSERT INTO employee_dob VALUES (12358,	'1992-06-08');
+INSERT INTO employee_dob VALUES (12349,	'1992-04-05');
+INSERT INTO employee_dob VALUES (12350,	'1988-12-31');
