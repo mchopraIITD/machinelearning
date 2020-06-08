b6c5c0237a81224b767de8fd4f309bc847db663b
mchopra <manan.chopra@gmail.com>
2020-06-08 16:07:26 -0400

    add customer item table

---
+++ sql/data/customer_item.sql
@@ -0,0 +1,6 @@
+CREATE TABLE customer_item (customer_id VARCHAR(50), first_name VARCHAR(50), last_name VARCHAR(50), city VARCHAR(50), state VARCHAR(10));
+INSERT INTO customer_item VALUES ('C_12341', 'Jack', 'Ryan', 'Seattle', 'WA');
+INSERT INTO customer_item VALUES ('C_12342', 'Joe', 'Watson', 'Seattle', 'WA');
+INSERT INTO customer_item VALUES ('C_12343', 'Bill', 'Willson', 'New York', 'NY');
+INSERT INTO customer_item VALUES ('C_12344', 'Bill', 'Hilton', 'San Diego', 'CA');
+
