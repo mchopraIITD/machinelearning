e01de6b31cac8b612ffb27dc18e055b2aa18c001
mchopra <manan.chopra@gmail.com>
2020-06-08 16:07:39 -0400

    Add item table

---
+++ sql/data/itemTable.sql
@@ -0,0 +1,5 @@
+CREATE TABLE itemTable (item_id VARCHAR(50), customer_id VARCHAR(50), quantity INTEGER, order_date DATE);
+INSERT INTO itemTable VALUES ('12345_a1', 'C_12341', 1, '2020-05-24');
+INSERT INTO itemTable VALUES ('12345_a2', 'C_12342', 2, '2020-05-24');
+INSERT INTO itemTable VALUES ('12345_a3', 'C_12343', 3, '2020-05-25');
+INSERT INTO itemTable VALUES ('12345_a4', 'C_12344', 1, '2020-05-25');
