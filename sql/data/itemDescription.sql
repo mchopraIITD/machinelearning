de505ef5b90d70c0899cb1c9214ca978a2d64712
mchopra <manan.chopra@gmail.com>
2020-06-08 15:54:09 -0400

    add itemDescription dataset

---
+++ sql/data/itemDescription.sql
@@ -0,0 +1,5 @@
+CREATE TABLE itemDescription (item_id VARCHAR(50), price DECIMAL (10, 2), description VARCHAR(50));
+INSERT INTO itemDescription VALUES('12345_a1',400,'camera');
+INSERT INTO itemDescription VALUES('12345_a2',200,'video game');
+INSERT INTO itemDescription VALUES('12345_a3',150,'video game');
+INSERT INTO itemDescription VALUES('12345_a4',300,'video game'); 
