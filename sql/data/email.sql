84adc9ec58322f15d4d89bd93af3519c35d7a54b
mchopra <manan.chopra@gmail.com>
2020-06-09 11:28:16 -0400

    Email dataset

---
+++ sql/data/email.sql
@@ -0,0 +1,6 @@
+CREATE TABLE email (id INTEGER, name VARCHAR(50), email VARCHAR(50));
+INSERT INTO email VALUES (1, 'John', 'asd@asd.com');
+INSERT INTO email VALUES (2, 'Sam', 'asd@asd.com');
+INSERT INTO email VALUES (3, 'Tom',  'asd@asd.com');
+INSERT INTO email VALUES (4, 'Bob', 'bob@asd.com');
+INSERT INTO email VALUES (5, 'Tom', 'asd@asd.com');
