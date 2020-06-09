e41590414e6786e2cb129c6acb291f3bad7c3990
mchopra <manan.chopra@gmail.com>
2020-06-09 10:43:38 -0400

    Distance dataset

---
+++ sql/data/distance.sql
@@ -0,0 +1,7 @@
+CREATE TABLE distance (sequence INTEGER, city VARCHAR(50), miles INTEGER);
+INSERT INTO distance VALUES (1, 'Seattle', 0);
+INSERT INTO distance VALUES (2,	'Portland',	174);
+INSERT INTO distance VALUES (3, 'San Francisco', 808);
+INSERT INTO distance VALUES (4, 'Monterey',	926);
+INSERT INTO distance VALUES (5, 'Los Angeles', 1251);
+INSERT INTO distance VALUES (6,	'San Diego', 1372);
