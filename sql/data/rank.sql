1fe80c42a34f38c7dafc0071f5740f807c9f83d2
mchopra <manan.chopra@gmail.com>
2020-06-09 15:24:07 -0400

    Add rank test

---
+++ sql/data/rank.sql
@@ -0,0 +1,10 @@
+CREATE TABLE rank_test (Name VARCHAR(50), Department VARCHAR(50), Sales INTEGER);
+INSERT INTO rank_test VALUES ('Manan', 'Hardware', 100);
+INSERT INTO rank_test VALUES ('Bob', 'Software',10);
+INSERT INTO rank_test VALUES ('John', 'Hardware', 50);
+INSERT INTO rank_test VALUES ('Mike', 'Software', 10);
+INSERT INTO rank_test VALUES ('Katie', 'Software', 50);
+INSERT INTO rank_test VALUES ('Cole', 'Software', 50);
+INSERT INTO rank_test VALUES ('Ari', 'Hardware', 60);
+INSERT INTO rank_test VALUES ('Piyush', 'Hardware', 70);
+
